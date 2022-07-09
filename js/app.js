e032b96b4c6b7be05563deed43422e58ae104f0d
ndukasamuel <104394603+ndukasamuel@users.noreply.github.com>
2022-07-09 19:25:59 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -39,18 +39,3 @@ link.onclick = function () {
     }
 }
 
-
-
-
-
-// const password = document.getElementById("password");
-//     const togglePassword = document.getElementById("toggle-password");
-//     togglePassword.addEventListener("click", toggleClicked);
-// function toggleClicked() {
-//     password.classList.toggle("visible");
-//     if (this.checked) {
-//         password.type = "text";
-//     } else {
-//         password.type = "password";
-//     }
-// }
